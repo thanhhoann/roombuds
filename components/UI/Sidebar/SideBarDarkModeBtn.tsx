07851b21e3ff03db3c660539edb44691bc066d19
thanhhoan <pdth456@gmail.com>
2021-09-13 22:19:24 +0700

    fixed dark mode rendering and hydration mismatch error

--- components/UI/Sidebar/SideBarDarkModeBtn.tsx
+++ components/UI/Sidebar/SideBarDarkModeBtn.tsx
@@ -1,31 +1,29 @@
 import { Switch } from "@headlessui/react";
-import { useRecoilState } from "recoil";
-import { darkModeEnabled } from "../../../atoms";
 import { moonSVG, sunSVG } from "../../../assets/SideBarDarkModeBtnSVG";
 import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
 
 export default function MyToggle() {
-  const [enabled, setEnabled] = useRecoilState(darkModeEnabled);
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  // only renders UI that uses the current theme when mounted ( fix hydration mismatch error )
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
 
   return (
     <>
       <div className="m-10 flex dark:text-background">
-        <div className="mr-2">{enabled ? moonSVG : sunSVG}</div>
+        <div className="mr-2">{theme === "dark" ? moonSVG : sunSVG}</div>
 
         <Switch
-          checked={enabled}
-          onChange={() => {
-            setTheme(theme === "dark" ? "light" : "dark");
-            setEnabled(!enabled);
-          }}
-          className={`${
-            enabled ? "bg-background_dark" : "bg-main"
-          } relative inline-flex items-center h-6 rounded-full w-16 border-2`}
+          checked={theme === "dark" ? true : false}
+          onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+          className={`bg-main dark:bg-background_dark relative inline-flex items-center h-6 rounded-full w-16 border-2`}
         >
           <span
             className={`${
-              enabled ? "translate-x-10" : "translate-x-1"
+              theme === "dark" ? "translate-x-10" : "translate-x-1"
             } inline-block w-4 h-4 transform bg-btn_text dark:bg-btn_dark rounded-full`}
           />
         </Switch>
