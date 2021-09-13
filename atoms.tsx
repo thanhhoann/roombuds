d795313960c5cddf95e54e9f3bcb85382a007933
thanhhoan <pdth456@gmail.com>
2021-09-13 20:43:55 +0700

    testing production mode

--- atoms.tsx
+++ atoms.tsx
@@ -1,12 +1,14 @@
 import { atom } from "recoil";
+//  import { recoilPersist } from "recoil-persist";
+
+//  const { persistAtom } = recoilPersist();
 
 export const sideBarItemsSelected = atom({
-  key: "sideBarItemsSelected", 
-  default: "Home", 
+  key: "sideBarItemsSelected",
+  default: "Home",
 });
 
 export const darkModeEnabled = atom({
   key: "darkModeEnabled",
   default: false,
 });
-
