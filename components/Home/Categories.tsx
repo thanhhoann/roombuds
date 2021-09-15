a6613288978c1b4339076438e96802b1402b53c2
thanhhoan <pdth456@gmail.com>
2021-09-15 14:17:59 +0700

    add style to categories title

--- components/Home/Categories.tsx
+++ components/Home/Categories.tsx
@@ -7,7 +7,9 @@ export default function Categories() {
       <div className="flex flex-col text-para row-start-3 col-start-3 col-span-8">
         <main className="grid place-items-center laptop:flex">
           <h1 className="font-extrabold rounded-md p-3 tracking-normal text-center laptop:text-left text-4xl laptop:text-5xl laptop:ml-20 dark:text-headline_dark flex">
-            <span className="grid place-items-center bg-main rounded-md p-3 mr-3">{CategoriesSVG}</span>
+            <span className="grid place-items-center bg-main dark:bg-background_dark dark:text-headline_dark rounded-md p-3 mr-3 border-2 dark:border-headline_dark">
+              {CategoriesSVG}
+            </span>
             Categories
           </h1>
         </main>
