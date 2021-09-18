b32508f00fe5b743a321c6c0e7b0b48db0146e5d
thanhhoan <pdth456@gmail.com>
2021-09-18 21:07:07 +0700

    added elec-and-water page and renting page

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -5,10 +5,6 @@ const Account: NextPage = () => {
   return (
     <>
       <Layout title="Account">
-        <div className="bg-main rounded-md p-3 text-2xl text-center m-10">Change username</div>
-        <div className="bg-main rounded-md p-3 text-2xl text-center m-10">Change password</div>
-        <div className="bg-main rounded-md p-3 text-2xl text-center m-10">Change bio</div>
-        <div className="bg-main rounded-md p-3 text-2xl text-center m-10">Log out</div>
       </Layout>
     </>
   );
